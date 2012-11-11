b92dd200752daccb8e509835909490fbf12ebf55
newrookie <shionw@comcast.net>
2012-11-11 00:00:07 -0800

    Change audio to play file so that the caller hears the music and not the person receiving the call

---
+++ gen/com/Aeon/C/Concept/my/contact/tones/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.Aeon.C.Concept.my.contact.tones;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
